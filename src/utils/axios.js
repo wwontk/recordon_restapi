98ea936d7280a297e58b02957277d7d5ea4b1c7b
wwontk <wwontk@gmail.com>
2025-01-24 08:10:29 +0900

    feat: Add cookie management and Axios utility functions

---
+++ src/utils/axios.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+import { getCookie } from "./cookie";
+
+const axiosInstance = axios.create({
+  baseURL: `${import.meta.env.VITE_APP_PUBLIC_SERVER}`,
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = getCookie("access-token");
+    config.headers["Content-Type"] = "application/json";
+    config.headers.Authorization = `Bearer ${token}`;
+    return config;
+  },
+  (error) => {
+    console.log(error);
+  }
+);
