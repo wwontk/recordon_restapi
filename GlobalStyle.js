e841fc5c2098d7e06b7d0535c8f4313e4502c533
wwontk <wwontk@gmail.com>
2025-01-23 09:57:14 +0900

    chore: Install essential libraries for project setup

---
+++ GlobalStyle.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+ * {
+    box-sizing: border-box; /* 모든 요소에 적용 */
+    margin: 0;
+    padding: 0;
+  }`;
+
+export default GlobalStyle;
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import GlobalStyle from "../GlobalStyle";
 
 function App() {
   return (
     <>
-      <div></div>
+      <GlobalStyle />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
   );
 }
