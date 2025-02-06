86006dd536870b5f228894ffdca01f0eb7db8b95
wwontk <wwontk@gmail.com>
2025-02-06 09:19:04 +0900

    feat: 회사 리스트 조회 더보기 버튼 구현

---
+++ src/components/Content/CompanyListContent/data.js
@@ -0,0 +1,16 @@
+export const CompanyList = [
+  {
+    companyId: "testbp",
+    salesresp: "비플",
+    companyName: "비플",
+    companyNumber: 2,
+    businessNumber: 123123,
+  },
+  {
+    companyId: "wwontk",
+    salesresp: "연구소",
+    companyName: "연구소",
+    companyNumber: 23,
+    businessNumber: 23153,
+  },
+];
--- src/components/Content/CompanyListContent/index.jsx
+++ src/components/Content/CompanyListContent/index.jsx
@@ -1,64 +1,79 @@
+import { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
+import MoreIcon from "../../../assets/img/etc/more-vertical.png";
+import "react-sliding-side-panel/lib/index.css";
+import { CompanyList } from "./data";
 
 const CompanyListContent = () => {
+  const [selectedCompany, setSelectedCompany] = useState(""); // 선택된 회사 ID
+  const menuRef = useRef(null);
+
+  // 바깥 클릭 감지 -> 메뉴 닫기
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setSelectedCompany("");
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  // 더보기 버튼 클릭 이벤트
+  const handleMoreClick = (companyId) => {
+    setSelectedCompany((prev) => (prev === companyId ? "" : companyId));
+  };
+
   return (
-    <>
-      <ContentContainer>
-        <table>
-          <thead>
-            <tr>
-              <th>회사번호</th>
-              <th>회사이름</th>
-              <th>회사아이디</th>
-              <th>사업자번호</th>
-              <th>영업점</th>
+    <ContentContainer>
+      <table>
+        <thead>
+          <tr>
+            <th>회사번호</th>
+            <th>회사이름</th>
+            <th>회사아이디</th>
+            <th>사업자번호</th>
+            <th>영업점</th>
+          </tr>
+        </thead>
+        <tbody>
+          {CompanyList.map((list) => (
+            <tr key={list.companyId}>
+              <td>{list.companyNumber}</td>
+              <td>{list.companyName}</td>
+              <td>{list.companyId}</td>
+              <td>{list.businessNumber}</td>
+              <td>
+                <p>{list.salesresp}</p>
+                <MoreButton
+                  ref={selectedCompany === list.companyId ? menuRef : null}
+                >
+                  <img
+                    src={MoreIcon}
+                    alt="more"
+                    onClick={() => handleMoreClick(list.companyId)}
+                  />
+                  {selectedCompany === list.companyId && (
+                    <DropdownMenu>
+                      <li>상세조회</li>
+                      <li>삭제</li>
+                    </DropdownMenu>
+                  )}
+                </MoreButton>
+              </td>
             </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>2</td>
-              <td>비플</td>
-              <td>bptest</td>
-              <td>111222333</td>
-              <td>비플</td>
-            </tr>
-            <tr>
-              <td>2</td>
-              <td>비플</td>
-              <td>bptest</td>
-              <td>111222333</td>
-              <td>비플</td>
-            </tr>
-            <tr>
-              <td>2</td>
-              <td>비플</td>
-              <td>bptest</td>
-              <td>111222333</td>
-              <td>비플</td>
-            </tr>
-            <tr>
-              <td>2</td>
-              <td>비플</td>
-              <td>bptest</td>
-              <td>111222333</td>
-              <td>비플</td>
-            </tr>
-            <tr>
-              <td>2</td>
-              <td>비플</td>
-              <td>bptest</td>
-              <td>111222333</td>
-              <td>비플</td>
-            </tr>
-          </tbody>
-        </table>
-      </ContentContainer>
-    </>
+          ))}
+        </tbody>
+      </table>
+    </ContentContainer>
   );
 };
 
 export default CompanyListContent;
 
+// 스타일 정의
 const ContentContainer = styled.div`
   width: 100%;
   height: calc(100% - 240px);
@@ -127,16 +142,79 @@ const ContentContainer = styled.div`
     // tr th:last-child {
     //   width: 125px;
     // }
-    // tr td:last-child {
-    //   position: relative;
-    //   height: 100%;
-    //   display: flex;
-    //   justify-content: space-between;
-    //   align-items: center;
-    //   & > p:last-child {
-    //     width: 26px;
-    //     height: 26px;
-    //   }
-    // }
+    tr td:last-child {
+      // position: relative;
+      // height: 100%;
+      // display: flex;
+      // justify-content: space-between;
+      // align-items: center;
+      // & > p:last-child {
+      //   width: 26px;
+      //   height: 26px;
+      // }
+
+      height: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+
+      & > img {
+        width: 28px;
+        height: 28px;
+        cursor: pointer;
+        transition: background-color 0.3s ease;
+        padding: 2px;
+        border-radius: 2px;
+
+        &:hover {
+          background-color: #e9e9e9;
+        }
+      }
+    }
+  }
+`;
+const MoreButton = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  & > img {
+    width: 28px;
+    height: 28px;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+    padding: 2px;
+    border-radius: 2px;
+
+    &:hover {
+      background-color: #e9e9e9;
+    }
+  }
+`;
+const DropdownMenu = styled.ul`
+  position: absolute;
+  top: 30px;
+  right: 0;
+  background: white;
+  border: 1px solid #d0d0d0;
+  box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
+  list-style: none;
+  width: 100px;
+  z-index: 10;
+  display: flex;
+  flex-direction: column;
+
+  li {
+    height: 40px;
+    cursor: pointer;
+    transition: background 0.2s ease;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    &:hover {
+      background: #f0f0f0;
+    }
   }
 `;
