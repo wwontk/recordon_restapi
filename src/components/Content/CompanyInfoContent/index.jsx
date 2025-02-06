fc219048502176bd8b1961148653df2925759a41
wwontk <wwontk@gmail.com>
2025-02-06 15:42:10 +0900

    feat: 회사 상세정보 퍼블리싱

---
+++ src/components/Content/CompanyInfoContent/index.jsx
@@ -0,0 +1,96 @@
+import styled from "styled-components";
+import { TextInput } from "../../Common/Input/TextInput";
+
+const CompanyInfoContent = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <>
+      <ContentContainer>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <div>
+              <label htmlFor="companyName">회사이름</label>
+              <InfoInput type="text" id="companyName" name="companyName" />
+            </div>
+            <div>
+              <label htmlFor="companyId">회사아이디</label>
+              <InfoInput type="text" id="companyId" name="companyId" />
+            </div>
+            <div>
+              <label htmlFor="companyPassword">비밀번호</label>
+              <InfoInput
+                type="password"
+                id="companyPassword"
+                name="companyPassword"
+              />
+            </div>
+            <div>
+              <label htmlFor="companyNumber">회사번호</label>
+              <InfoInput type="text" id="companyNumber" name="companyNumber" />
+            </div>
+            <div>
+              <label htmlFor="businessNumber">사업자번호</label>
+              <InfoInput
+                type="text"
+                id="businessNumber"
+                name="businessNumber"
+              />
+            </div>
+            <div>
+              <label htmlFor="salesresp">영업점</label>
+              <InfoInput type="text" id="salesresp" name="salesresp" />
+            </div>
+          </div>
+          <button>수정</button>
+        </form>
+      </ContentContainer>
+    </>
+  );
+};
+
+export default CompanyInfoContent;
+
+const ContentContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: #f8f8f8;
+  padding: 64px 80px 0;
+
+  & > form {
+    & > div {
+      display: flex;
+      flex-direction: column;
+      gap: 8px;
+      & > div {
+        display: flex;
+        align-items: center;
+        & > label {
+          width: 100px;
+        }
+      }
+    }
+    & > button {
+      margin-top: 60px;
+      width: 60px;
+      height: 36px;
+      background-color: white;
+      border: 1px solid #ce296f;
+      color: #ce296f;
+      cursor: pointer;
+      font-family: "42dot Sans", serif;
+      font-size: 16px;
+      border-radius: 2px;
+    }
+  }
+`;
+
+const InfoInput = styled(TextInput)`
+  width: 280px;
+  height: 30px;
+  padding-left: 12px;
+  border-radius: 0;
+  font-family: "42dot Sans", serif;
+  font-size: 16px;
+`;
--- src/pages/RecordON/List/Info/index.jsx
+++ src/pages/RecordON/List/Info/index.jsx
@@ -1,9 +1,62 @@
+import styled from "styled-components";
+import BackIcon from "../../../../assets/img/etc/chevrons-left.png";
+import { useNavigate } from "react-router-dom";
+import CompanyInfoContent from "../../../../components/Content/CompanyInfoContent";
+
 const Info = () => {
+  const navigate = useNavigate();
+
   return (
     <>
-      <p>인포</p>
+      <CompanyInfoContainer>
+        <CompanyInfoTop>
+          <div onClick={() => navigate(-1)}>
+            <img src={BackIcon} alt="back" />
+          </div>
+          <p>상세 조회</p>
+        </CompanyInfoTop>
+        <CompanyInfoContent />
+      </CompanyInfoContainer>
     </>
   );
 };
 
 export default Info;
+
+const CompanyInfoContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  font-family: "42dot Sans", serif;
+`;
+
+const CompanyInfoTop = styled.div`
+  height: 100px;
+  display: flex;
+  align-items: center;
+  padding: 0 0 0 80px;
+  position: relative;
+
+  & > div {
+    position: absolute;
+    left: 10px;
+    border-radius: 2px;
+    cursor: pointer;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    transition: background-color 0.3s ease;
+
+    &:hover {
+      background-color: #e9e9e9;
+    }
+
+    img {
+      width: 32px;
+      height: 32px;
+    }
+  }
+
+  & > p {
+    font-size: 20px;
+  }
+`;
