992ea311533d27b20125a59497d7767f8fb31f78
wwontk <wwontk@gmail.com>
2025-02-11 13:48:54 +0900

    chore: 리액트쿼리 라이브러리 설치

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,20 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { CookiesProvider } from "react-cookie";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <CookiesProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </CookiesProvider>
-  </StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <StrictMode>
+      <CookiesProvider>
+        <BrowserRouter>
+          <App />
+          <ReactQueryDevtools initialIsOpen />
+        </BrowserRouter>
+      </CookiesProvider>
+    </StrictMode>
+  </QueryClientProvider>
 );
